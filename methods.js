fb13cd147752af4eff5f4a9373f3a01c39d98f41
eye-opener-tenzin <tmejay.tm@gmail.com>
2020-10-26 22:35:21 -0700

    add methods exercise file

---
+++ methods.js
@@ -0,0 +1,10 @@
+const square = {
+    area: function(num) {
+        return num * num;
+    },
+    perimeter: function(num) {
+        return  num * 4;
+    }
+} 
+console.log(square.area(10));
+console.log(square.perimeter(10))
